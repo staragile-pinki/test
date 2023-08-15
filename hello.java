4e838666432c7417bd68164b82b3dd615e935f1d
PKPATEL83 <113413903+PKPATEL83@users.noreply.github.com>
2023-08-15 09:08:29 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-		System.out.println("Hello Pinki Patel");
-	    	System.out.println("Thank You");
+	System.out.println("Hello Pinki");
     }
 }
